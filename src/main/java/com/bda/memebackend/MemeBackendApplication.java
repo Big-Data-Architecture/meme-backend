32b4de1bba9b119ca9d7370706e362f9c2407f16
Rajiv Vangala <vangalarajiv@gmail.com>
2023-03-21 16:51:26 -0600

    project initialisation

---
+++ src/main/java/com/bda/memebackend/MemeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bda.memebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bda/memebackend/MemeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bda.memebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
