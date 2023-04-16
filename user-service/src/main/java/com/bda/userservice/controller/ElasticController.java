f65b8885bfa5ee5e35ab43b9e7bc647dcd6eefd8
Rakshith Jayanna <rakshithj1997@gmail.com>
2023-04-16 17:45:32 -0600

    Added elastic service and controller (#7)

---
+++ user-service/src/main/java/com/bda/userservice/controller/ElasticController.java
@@ -0,0 +1,72 @@
+package com.bda.userservice.controller;
+
+import com.bda.userservice.service.ElasticService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/search")
+public class ElasticController {
+
+    @Autowired
+    ElasticService elasticService;
+
+    private final Logger logger = LoggerFactory.getLogger(ElasticController.class.getSimpleName());
+
+    @GetMapping("/memes/")
+    public ResponseEntity<String> searchMemes(@RequestParam(value = "query", required = true) String query, @RequestParam(value = "type") String type) {
+        if (query.isBlank()) return ResponseEntity.badRequest().build();
+
+        try {
+            boolean isModelUp = elasticService.isModelUpAndRunning();
+            String responseBody = "";
+            if (type.equals("ml")) {
+                if (isModelUp) {
+                    responseBody = elasticService.knnSearch(query);
+                } else {
+                    throw new IOException("Model is not up");
+                }
+            } else {
+                responseBody = elasticService.generalSearch(query);
+            }
+            logger.debug(responseBody);
+            return ResponseEntity.ok(responseBody);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/meme_of_the_day")
+    public ResponseEntity<String> getMemeOfTheDay() {
+        try {
+            String response = elasticService.searchRandom();
+            logger.debug(response);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/meme_info/")
+    public ResponseEntity<String> getMemeByIds(@RequestParam("ids") List<String> ids) {
+        try {
+            String response = elasticService.getMemesByIds(ids);
+            logger.debug(response);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
