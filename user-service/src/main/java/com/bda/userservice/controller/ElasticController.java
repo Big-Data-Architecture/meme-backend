5c0ec52dcd44218fe0d66e28e65b6c676ca50106
Rajiv Vangala <vangalarajiv@gmail.com>
2023-04-23 02:06:37 -0600

    modified cors configuration

--- user-service/src/main/java/com/bda/userservice/controller/ElasticController.java
+++ user-service/src/main/java/com/bda/userservice/controller/ElasticController.java
@@ -5,14 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(originPatterns = "*", allowCredentials = "true", exposedHeaders = {"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"}, allowedHeaders = "*", methods = {RequestMethod.GET,RequestMethod.OPTIONS,RequestMethod.POST,RequestMethod.HEAD})
 @RequestMapping("api/v1/search")
 public class ElasticController {
 
