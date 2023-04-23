96b9f4f3411e3379ea4957eb158d638376154b58
Rajiv Vangala <vangalarajiv@gmail.com>
2023-04-22 18:56:33 -0600

    added kubernetes files and gateway server

--- build.sh
+++ build.sh
@@ -4,15 +4,18 @@ docker build -t us-central1-docker.pkg.dev/big-data-architecture-202303/meme-bac
 docker build -t us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/eureka-server:latest ./eureka-server
 docker build -t us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/user-service:latest ./user-service
 docker build -t us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/elastic-service:latest ./elastic-service
+docker build -t us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/gateway-server:latest ./gateway-server
 
 docker tag us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/config-server:latest us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/config-server:latest
 docker tag us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/eureka-server:latest us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/eureka-server:latest
 docker tag us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/user-service:latest us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/user-service:latest
 docker tag us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/elastic-service:latest us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/elastic-service:latest
+docker tag us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/gateway-server:latest us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/gateway-server:latest
 
 docker push us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/config-server:latest
 docker push us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/eureka-server:latest
 docker push us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/user-service:latest
 docker push us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/elastic-service:latest
+docker push us-central1-docker.pkg.dev/big-data-architecture-202303/meme-backend/gateway-server:latest
 
-docker-compose up
+docker-compose -f ./docker/docker-compose.yml up
