e308f2c4fbb6cf404f0630ab4e1fff4d71468e40
Rajiv Vangala <rajiv.vangala@colorado.edu>
2023-04-23 20:46:58 -0600

    Added redis cache (#23)

--- user-service/src/main/java/com/bda/userservice/controller/FavouritesController.java
+++ user-service/src/main/java/com/bda/userservice/controller/FavouritesController.java
@@ -5,6 +5,7 @@ import com.bda.userservice.dto.UserFavouritesDto;
 import com.bda.userservice.dto.IsFavouritesEntityDto;
 import com.bda.userservice.service.FavouritesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,11 +23,13 @@ public class FavouritesController {
     }
 
     @GetMapping(value = {"/favourites/{userId}"})
+    @Cacheable("favourites")
     public ResponseEntity<UserFavouritesDto> getFavourites(@PathVariable("userId") Integer userId){
         return ResponseEntity.ok(favouritesService.getFavourites(userId));
     }
 
     @GetMapping("/favourites/{userId}/{memeId}")
+    @Cacheable("isFavourites")
     public ResponseEntity<IsFavouritesEntityDto> isFavourites(@PathVariable("userId") Integer userId,
                                                           @PathVariable("memeId") String memeId){
         return ResponseEntity.ok(favouritesService.isFavourite(userId,memeId));
